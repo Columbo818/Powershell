9d56ead32277f5b6da9af12bfe12951321710e0e
Columbo818 <charlie@charliemiller.me.uk>
2018-10-27 14:19:40 +0100

    Add files via upload

---
+++ AD_DisplayNames.ps1
@@ -0,0 +1,16 @@
+﻿### Some Names in AD are usernames, some are display names?
+### This converts them all to display names.
+### Remove hashes, run it, replace the hashes for safety.
+
+
+Get-ADUser -Filter * -Properties * -SearchBase "OU=SITE, OU=Establishments, DC=SITE, DC=DOMAIN, DC=2LD, DC=TLD" | Select-Object sAMAccountName, cn, Givenname, Surname |
+  ForEach-Object{
+  $user = $_.sAMAccountName
+  $forename = $_.Givenname
+  $surname = $_.Surname
+  [string]$fullname = $forename + " " + $surname
+    #Set-ADUser $user -DisplayName $fullname -PassThru |
+    #Write-Host $user, $fullname
+    #Rename-ADObject -NewName $fullname
+    #Write-Host "$user Display Name and AD Name set sucessfully" -ForegroundColor Green
+    }
---
+++ SetMail.ps1
@@ -0,0 +1,8 @@
+﻿### Remove the Hash symbol on lines 6 and 7 to set the email field. Leave it in place to see a list of users with NO DATA in their email field. ###
+
+Get-ADUser -filter * -Properties * -SearchBase "OU=Users, OU=SITE, OU=Establishments, DC=SITE, DC=2LD, DC=TLD" | ? {$_.EmailAddress -eq $null} | Select-Object sAMAccountName |
+ForEach-Object{
+$mail = $_.sAMAccountName + "@SITE.org.uk"
+#Set-ADUser -Identity $_.sAMAccountName -EmailAddress $mail
+#Write-Host "Set email for $user to $mail"
+}
