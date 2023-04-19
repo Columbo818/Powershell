0e2cf88e92bec8b06328b34c2f9521a0d461f2de
Charles Miller <charlie@charliemiller.me.uk>
2023-04-19 08:18:55 +0100

    Add WUpdate.ps1
    Ignores group policy settings and searches online for windows updates. Used when there's an issue with on-prem WSUS or SCCM as a source for updates.

---
+++ WUpdate.ps1
@@ -0,0 +1,34 @@
+﻿if (!
+    # Get Current User
+    (New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent()
+    # Are they admin?
+    )).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+) {
+    # Reload script and run as admin
+    Start-Process `
+        -FilePath 'powershell' `
+        -ArgumentList (
+            # Flatten to single array
+            '-File', $MyInvocation.MyCommand.Source, $args `
+            | %{ $_ }
+        ) `
+        -Verb RunAs
+    exit
+}
+
+$wshell = New-Object -ComObject Wscript.Shell
+
+Set-ItemProperty -Path HKLM:\Software\Policies\Microsoft\Windows\WindowsUpdate -Name DisableWindowsUpdateAccess -Value 0 -Force
+Set-ItemProperty -Path HKLM:\Software\Policies\Microsoft\Windows\WindowsUpdate -Name DoNotConnectToWindowsUpdateInternetLocations -Value 0 -Force
+Set-ItemProperty -Path HKLM:\Software\Policies\Microsoft\Windows\WindowsUpdate -Name DisableDualScan -Value 0 -Force
+Set-ItemProperty -Path HKLM:\Software\Policies\Microsoft\Windows\WindowsUpdate -Name WUServer -Value 0 -Force
+Set-ItemProperty -Path HKLM:\Software\Policies\Microsoft\Windows\WindowsUpdate -Name WUStatusServer -Value 0 -Force
+Set-ItemProperty -Path HKLM:\Software\Policies\Microsoft\Windows\WindowsUpdate\AU -Name UseWUServer -Value 0 -Force
+Restart-Service -Name wuauserv -Force
+
+usoclient startinteractivescan
+Start-Process ms-settings:windowsupdate
+
+$wshell.Popup("Allow all of these updates to download & install, then restart your PC when promted. After the restart, printing will work as normal.")
+
+New-Item -Path C:\ -ItemType File -Name PrintFix.txt -Value "1" -Force
